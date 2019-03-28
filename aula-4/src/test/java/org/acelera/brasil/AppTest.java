9a1d4bfddf1c2118b6449339e296438d906c9919
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-28 18:17:33 -0300

    adiciona codigo

---
+++ aula-4/src/test/java/org/acelera/brasil/AppTest.java
@@ -0,0 +1,16 @@
+package org.acelera.brasil;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
